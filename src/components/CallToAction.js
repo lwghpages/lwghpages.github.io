4faff34d26b036c37768d830d8f4135ba83f8817
lwghpages <lwghpages@gmail.com>
2019-10-17 17:21:08 -0500

    Update CallToAction.js

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -9,11 +9,11 @@ export default class CallToAction extends Component {
       <div className="container">
         <div className="row align-items-center">
           <div className="col-lg-6 col-md-8" style={{padding:'5px 7px 5px 7px'}}>
-            <h3 className="sec-title"><span>Hire Me!</span> Get a Feel For 
+            <h3 className="sec-title"><span style={{color:'rgb(68, 153, 255)'}}>Hire Me!</span> Get a Feel For 
               My Work &amp; What I Can Do For / With Your Business</h3>
           </div>
           <div className="col-lg-6 col-md-4 text-center">
-            <a href="#" className="btn btnSeeWork" >Download My Resume<span style={{fontSize:'0.75em',padding:'0 7px 0 7px'}}> (PDF)</span></a>
+            <a href="#" className="btn btnSeeWork" style={{backgroundColor:'#001f61',color:'white',}} >Download My Resume<span style={{fontSize:'0.75em',padding:'0 7px 0 7px'}}> (PDF)</span></a>
           </div>
         </div>
       </div>
